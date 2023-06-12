e0f13e4c0bcf4159e5c88f8d1de13719fa01202c
Leonid Sulin <sulin.leonid@gmail.com>
2023-06-12 10:47:21 +0300

    Add buttons to OutputFrame for scaling SolutionPanel

--- src/OutputFrame.java
+++ src/OutputFrame.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class OutputFrame extends JFrame {
     public OutputFrame(Solution solution) {
@@ -11,6 +13,26 @@ public class OutputFrame extends JFrame {
         JScrollPane scrollPane = new JScrollPane();
         scrollPane.setViewportView(solutionPanel);
         mainPanel.add(scrollPane, BorderLayout.CENTER);
+
+        JPanel controlPanel = new JPanel();
+        controlPanel.setLayout(new FlowLayout());
+        JButton zoomOutButton = new JButton("Отдалить");
+        zoomOutButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                solutionPanel.scaleFactor /= 1.1;
+                solutionPanel.repaint();
+            }
+        });
+        controlPanel.add(zoomOutButton);
+        JButton zoomInButton = new JButton("Приблизить");
+        zoomInButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                solutionPanel.scaleFactor *= 1.1;
+                solutionPanel.repaint();
+            }
+        });
+        controlPanel.add(zoomInButton);
+        mainPanel.add(controlPanel, BorderLayout.SOUTH);
 
         getContentPane().add(mainPanel);
         pack();
